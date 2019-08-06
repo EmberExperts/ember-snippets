062687e780bcbcbaac8b9e2260e4324460f77a64
Maciej Kwaśniak <kmaciek17@gmail.com>
2019-08-06 18:32:33 +0200

    Use ember namespace for hbs

--- src/handlebars/ember/components.js
+++ src/handlebars/ember/components.js
@@ -1,15 +1,15 @@
 module.exports = {
-  "[handelbars] input": {
+  "[ember] input": {
     "prefix": "input",
     "body": "<Input @value=${1} />",
     "description": "The {{input}} helper lets you create an HTML <input /> component."
   },
-  "[handelbars] textarea": {
+  "[ember] textarea": {
     "prefix": "textarea",
     "body": "<Textarea @value=${1} />",
     "description": "The {{textarea}} helper lets you create an HTML <textarea /> component."
   },
-  "[handelbars] link-to": {
+  "[ember] link-to": {
     "prefix": "link-to",
     "body": "<LinkTo @route=${1}>\n\t${2}\n</LinkTo>",
     "description": "The {{link-to}} component renders a link to the supplied routeName."
--- src/handlebars/ember/hbs-helpers.js
+++ src/handlebars/ember/hbs-helpers.js
@@ -1,70 +1,70 @@
 module.exports = {
-  "[handelbars] #each": {
+  "[ember] #each": {
     "prefix": "each",
     "body": "{{#each ${1} as |${2}|}}\n\t${3}\n{{/each}}",
     "description": "The {{#each}} helper loops over elements in a collection."
   },
-  "[handelbars] #each-in": {
+  "[ember] #each-in": {
     "prefix": "each-in",
     "body": "{{#each-in ${1} as |${2} ${3}|}}\n\t${4}\n{{/each-in}}",
     "description": "The {{each-in}} helper loops over properties on an object."
   },
-  "[handelbars] #each/else": {
+  "[ember] #each/else": {
     "prefix": "eachelse",
     "body": "{{#each ${1} as |${2}|}}\n\t${3}\n{{else}}\n\t${4}\n{{/each}}",
     "description": "The {{#each}} helper loops over elements in a collection."
   },
-  "[handelbars] #if hasBlock": {
+  "[ember] #if hasBlock": {
     "prefix": "ifhasBlock",
     "body": "{{#if (hasBlock)}}\n\t${1}\n{{/if}}",
     "description": "Indicates if the component was invoked with a block."
   },
-  "[handelbars] #if hasBlockParams": {
+  "[ember] #if hasBlockParams": {
     "prefix": "ifhasBlockParams",
     "body": "{{#if (hasBlockParams)}}\n\t${1}\n{{/if}}",
     "description": "Indicates if the component was invoked with block params."
   },
-  "[handelbars] if": {
+  "[ember] if": {
     "prefix": "if",
     "body": "{{if ${1} ${2} ${3}}}",
     "description": "The if helper allows you to conditionally render one of two branches, depending on the 'truthiness' of a property."
   },
-  "[handelbars] #if": {
+  "[ember] #if": {
     "prefix": "if",
     "body": "{{#if ${1}}}\n\t${3}\n{{/if}}",
     "description": "The if helper allows you to conditionally render one of two branches, depending on the 'truthiness' of a property."
   },
-  "[handelbars] #if/else": {
+  "[ember] #if/else": {
     "prefix": "if",
     "body": "{{#if ${1}}}\n\t${2}\n{{else}}\n\t${3}\n{{/if}}",
     "description": "The if helper allows you to conditionally render one of two branches, depending on the 'truthiness' of a property."
   },
-  "[handelbars] unless": {
+  "[ember] unless": {
     "prefix": "unless",
     "body": "{{unless ${1} ${2} ${3}}}",
-    "description": "[handelbars] unless"
+    "description": "[ember] unless"
   },
-  "[handelbars] #unless": {
+  "[ember] #unless": {
     "prefix": "unless",
     "body": "{{#unless ${1}}}\n\t${2}\n{{/unless}}",
     "description": "The unless helper is the inverse of the if helper. It displays if a value is falsey (\"not true\" or \"is false\"). "
   },
-  "[handelbars] #unless/else": {
+  "[ember] #unless/else": {
     "prefix": "unless",
     "body": "{{#unless ${1}}}\n\t${2}\n{{else}}\n\t${3}\n{{/unless}}",
     "description": "The unless helper is the inverse of the if helper. It displays if a value is falsey (\"not true\" or \"is false\"). "
   },
-  "[handelbars] with": {
+  "[ember] with": {
     "prefix": "with",
     "body": "{{#with ${1} as |${2}|}}\n\t${3}\n{{/with}}",
-    "description": "[handelbars] with"
+    "description": "[ember] with"
   },
-  "[handelbars] log": {
+  "[ember] log": {
     "prefix": "log",
     "body": "{{log ${1}}}",
     "description": "log allows you to output the value of variables in the current rendering context. log also accepts primitive types such as strings or numbers."
   },
-  "[handelbars] yield": {
+  "[ember] yield": {
     "prefix": "yield",
     "body": "{{yield}}",
     "description": "{{yield}} denotes an area of a template that will be rendered inside of another template."
