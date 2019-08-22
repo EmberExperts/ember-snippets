9b503a89af57886b21517490558b00bea40cba35
Maciej Kwaśniak <kmaciek17@gmail.com>
2019-08-22 22:07:39 +0200

    Dynamic ember data imports

--- src/javascript/addons/ember-data/imports.js
+++ src/javascript/addons/ember-data/imports.js
@@ -1,142 +1,41 @@
-module.exports = {
-  "[ember-data] Import Model": {
-    "prefix": "imModel",
-    "body": "import Model from '@ember-data/model';",
-    "description": "[ember-data] Import Model"
-  },
-  "[ember-data] Import attr": {
-    "prefix": "imattr",
-    "body": "import { attr } from '@ember-data/model';",
-    "description": "[ember-data] Import attr"
-  },
-  "[ember-data] Import belongsTo": {
-    "prefix": "imbelongsTo",
-    "body": "import { belongsTo } from '@ember-data/model';",
-    "description": "[ember-data] Import belongsTo"
-  },
-  "[ember-data] Import hasMany": {
-    "prefix": "imhasMany",
-    "body": "import { hasMany } from '@ember-data/model';",
-    "description": "[ember-data] Import hasMany"
-  },
-  "[ember-data] Import Adapter": {
-    "prefix": "imAdapter",
-    "body": "import Adapter from '@ember-data/adapter';",
-    "description": "[ember-data] Import Adapter"
-  },
-  "[ember-data] Import RESTAdapter": {
-    "prefix": "imRESTAdapter",
-    "body": "import RESTAdapter from '@ember-data/adapter/rest';",
-    "description": "[ember-data] Import RESTAdapter"
-  },
-  "[ember-data] Import JSONAPIAdapter": {
-    "prefix": "imJSONAPIAdapter",
-    "body": "import JSONAPIAdapter from '@ember-data/adapter/json-api';",
-    "description": "[ember-data] Import JSONAPIAdapter"
-  },
-  "[ember-data] Import BuildURLMixin": {
-    "prefix": "imBuildURLMixin",
-    "body": "import { BuildURLMixin } from '@ember-data/adapter';",
-    "description": "[ember-data] Import BuildURLMixin"
-  },
-  "[ember-data] Import AdapterError": {
-    "prefix": "imAdapterError",
-    "body": "import AdapterError from '@ember-data/adapter/error';",
-    "description": "[ember-data] Import AdapterError"
-  },
-  "[ember-data] Import InvalidError": {
-    "prefix": "imInvalidError",
-    "body": "import { InvalidError } from '@ember-data/adapter/error';",
-    "description": "[ember-data] Import InvalidError"
-  },
-  "[ember-data] Import TimeoutError": {
-    "prefix": "imTimeoutError",
-    "body": "import { TimeoutError } from '@ember-data/adapter/error';",
-    "description": "[ember-data] Import TimeoutError"
-  },
-  "[ember-data] Import AbortError": {
-    "prefix": "imAbortError",
-    "body": "import { AbortError } from '@ember-data/adapter/error';",
-    "description": "[ember-data] Import AbortError"
-  },
-  "[ember-data] Import UnauthorizedError": {
-    "prefix": "imUnauthorizedError",
-    "body": "import { UnauthorizedError } from '@ember-data/adapter/error';",
-    "description": "[ember-data] Import UnauthorizedError"
-  },
-  "[ember-data] Import ForbiddenError": {
-    "prefix": "imForbiddenError",
-    "body": "import { ForbiddenError } from '@ember-data/adapter/error';",
-    "description": "[ember-data] Import ForbiddenError"
-  },
-  "[ember-data] Import NotFoundError": {
-    "prefix": "imNotFoundError",
-    "body": "import { NotFoundError } from '@ember-data/adapter/error';",
-    "description": "[ember-data] Import NotFoundError"
-  },
-  "[ember-data] Import ConflictError": {
-    "prefix": "imConflictError",
-    "body": "import { ConflictError } from '@ember-data/adapter/error';",
-    "description": "[ember-data] Import ConflictError"
-  },
-  "[ember-data] Import ServerError": {
-    "prefix": "imServerError",
-    "body": "import { ServerError } from '@ember-data/adapter/error';",
-    "description": "[ember-data] Import ServerError"
-  },
-  "[ember-data] Import errorsHashToArray": {
-    "prefix": "imerrorsHashToArray",
-    "body": "import { errorsHashToArray } from '@ember-data/adapter/error';",
-    "description": "[ember-data] Import errorsHashToArray"
-  },
-  "[ember-data] Import errorsArrayToHash": {
-    "prefix": "imerrorsArrayToHash",
-    "body": "import { errorsArrayToHash } from '@ember-data/adapter/error';",
-    "description": "[ember-data] Import errorsArrayToHash"
-  },
-  "[ember-data] Import Serializer": {
-    "prefix": "imSerializer",
-    "body": "import Serializer from '@ember-data/serializer';",
-    "description": "[ember-data] Import Serializer"
-  },
-  "[ember-data] Import JSONSerializer": {
-    "prefix": "imJSONSerializer",
-    "body": "import JSONSerializer from '@ember-data/serializer/json';",
-    "description": "[ember-data] Import JSONSerializer"
-  },
-  "[ember-data] Import RESTSerializer": {
-    "prefix": "imRESTSerializer",
-    "body": "import RESTSerializer from '@ember-data/serializer/rest';",
-    "description": "[ember-data] Import RESTSerializer"
-  },
-  "[ember-data] Import JSONAPISerializer": {
-    "prefix": "imJSONAPISerializer",
-    "body": "import JSONAPISerializer from '@ember-data/serializer/json-api';",
-    "description": "[ember-data] Import JSONAPISerializer"
-  },
-  "[ember-data] Import EmbeddedRecordsMixin": {
-    "prefix": "imEmbeddedRecordsMixin",
-    "body": "import { EmbeddedRecordsMixin } from '@ember-data/serializer/rest';",
-    "description": "[ember-data] Import EmbeddedRecordsMixin"
-  },
-  "[ember-data] Import Transform": {
-    "prefix": "imTransform",
-    "body": "import Transform from '@ember-data/serializer/transform';",
-    "description": "[ember-data] Import Transform"
-  },
-  "[ember-data] Import Store": {
-    "prefix": "imStore",
-    "body": "import Store from '@ember-data/store';",
-    "description": "[ember-data] Import Store"
-  },
-  "[ember-data] Import normalizeModelName": {
-    "prefix": "imnormalizeModelName",
-    "body": "import { normalizeModelName } from '@ember-data/store';",
-    "description": "[ember-data] Import normalizeModelName"
-  },
-  "[ember-data] Import RecordData": {
-    "prefix": "imRecordData",
-    "body": "import RecordData from '@ember-data/record-data';",
-    "description": "[ember-data] Import RecordData"
+const data = require("@ember-data/rfc395-data");
+
+const customMapping = {};
+
+function getImport(object) {
+  if (object.replacement.export === "default") {
+    const mapping = customMapping[object.replacement.module] || {};
+    return mapping[object.localName] || object.localName;
+  } else {
+    const mapping = customMapping[object.replacement.module] || {};
+    const customName = mapping[object.replacement.export];
+    
+    if (customName) {
+      return `{ ${object.replacement.export} as ${customName} }`;
+    }
+    
+    return `{ ${object.replacement.export} }`;
   }
-};
+}
+
+function getDescriptor(object) {
+  if (object.localName) return object.localName;
+
+  if (object.replacement.export === "default") {
+    return object.export;
+  }
+
+  return object.replacement.export;
+}
+
+const imports = {};
+
+data.forEach((object) => {
+  imports[`[ember-data] ${getDescriptor(object)}`] = {
+    prefix: `im${object.localName || object.replacement.export}`,
+    body: `import ${getImport(object)} from '${object.replacement.module}';`,
+    description: getDescriptor(object)
+  };
+});
+
+module.exports = imports;
