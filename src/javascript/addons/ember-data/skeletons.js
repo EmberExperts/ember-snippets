48d0866a3abbb92d931b02aff626f8fd4c1773e8
Maciej Kwaśniak <kmaciek17@gmail.com>
2019-10-22 14:02:49 +0200

    Add ES6 skeletons

--- src/javascript/addons/ember-data/skeletons.js
+++ src/javascript/addons/ember-data/skeletons.js
@@ -4,39 +4,79 @@ module.exports = {
     "body": "import Model from '@ember-data/model';\n\nexport default Model.extend({\n\t${1:// body}\n});",
     "description": "Model Skeleton"
   },
+  "[ember-data] ES6 Model Skeleton": {
+    "prefix": "model",
+    "body": "import classic from 'ember-classic-decorator';\nimport Model from '@ember-data/model';\n\n@classic\nclass ${1}Model extends Model {\n\t${2}\n}\n\nexport default ${1}Model;",
+    "description": "ES6 Model Skeleton"
+  },
   "[ember-data] Adapter Skeleton": {
     "prefix": "adapter",
     "body": "import Adapter from '@ember-data/adapter';\n\nexport default Adapter.extend({\n\t${1:// body}\n});",
     "description": "Adapter Skeleton"
+  },
+  "[ember-data] ES6 Adapter Skeleton": {
+    "prefix": "adapter",
+    "body": "import classic from 'ember-classic-decorator';\nimport Adapter from '@ember-data/adapter';\n\n@classic\nclass ${1}Adapter extends Adapter {\n\t${2}\n}\n\nexport default ${1}Adapter;",
+    "description": "ES6 Adapter Skeleton"
   },
   "[ember-data] REST Adapter Skeleton": {
     "prefix": "restAdapter",
     "body": "import RESTAdapter from '@ember-data/adapter/rest';\n\nexport default RESTAdapter.extend({\n\t${1:// body}\n});",
     "description": "REST Adapter Skeleton"
   },
+  "[ember-data] ES6 REST Adapter Skeleton": {
+    "prefix": "restAdapter",
+    "body": "import classic from 'ember-classic-decorator';\nimport RESTAdapter from '@ember-data/adapter/rest';\n\n@classic\nclass ${1}RESTAdapter extends RESTAdapter {\n\t${2}\n}\n\nexport default ${1}RESTAdapter;",
+    "description": "ES6 REST Adapter Skeleton"
+  },
   "[ember-data] JSON API Adapter Skeleton": {
     "prefix": "jsonapiAdapter",
     "body": "import JSONAPIAdapter from '@ember-data/adapter/json-api';\n\nexport default JSONAPIAdapter.extend({\n\t${1:// body}\n});",
     "description": "JSON API Adapter Skeleton"
+  },
+  "[ember-data] ES6 JSON API Adapter Skeleton": {
+    "prefix": "jsonapiAdapter",
+    "body": "import classic from 'ember-classic-decorator';\nimport JSONAPIAdapter from '@ember-data/adapter/json-api';\n\n@classic\nclass ${1}JSONAPIAdapter extends JSONAPIAdapter {\n\t${2}\n}\n\nexport default ${1}JSONAPIAdapter;",
+    "description": "ES6 JSON API Adapter Skeleton"
   },
   "[ember-data] Serializer Skeleton": {
     "prefix": "serializer",
     "body": "import Serializer from '@ember-data/serializer';\n\nexport default Serializer.extend({\n\t${1:// body}\n});",
     "description": "Serializer Skeleton"
   },
+  "[ember-data] ES6 Serializer Skeleton": {
+    "prefix": "serializer",
+    "body": "import classic from 'ember-classic-decorator';\nimport Serializer from '@ember-data/serializer';\n\n@classic\nclass ${1}Serializer extends Serializer {\n\t${2}\n}\n\nexport default ${1}Serializer;",
+    "description": "ES6 Serializer Skeleton"
+  },
   "[ember-data] REST Serializer Skeleton": {
     "prefix": "restSerializer",
     "body": "import RESTSerializer from '@ember-data/serializer/rest';\n\nexport default RESTSerializer.extend({\n\t${1:// body}\n});",
     "description": "REST Serializer Skeleton"
+  },
+  "[ember-data] ES6 REST Serializer Skeleton": {
+    "prefix": "restSerializer",
+    "body": "import classic from 'ember-classic-decorator';\nimport RESTSerializer from '@ember-data/serializer/rest';\n\n@classic\nclass ${1}RESTSerializer extends RESTSerializer {\n\t${2}\n}\n\nexport default ${1}RESTSerializer;",
+    "description": "ES6 REST Serializer Skeleton"
   },
   "[ember-data] JSON API Serializer Skeleton": {
     "prefix": "jsonapiSerializer",
     "body": "import JSONAPISerializer from '@ember-data/serializer/json-api';\n\nexport default JSONAPISerializer.extend({\n\t${1:// body}\n});",
     "description": "JSON API Serializer Skeleton"
   },
+  "[ember-data] ES6 JSON API Serializer Skeleton": {
+    "prefix": "jsonapiSerializer",
+    "body": "import classic from 'ember-classic-decorator';\nimport JSONAPISerializer from '@ember-data/serializer/json-api';\n\n@classic\nclass ${1}JSONAPISerializer extends JSONAPISerializer {\n\t${2}\n}\n\nexport default ${1}JSONAPISerializer;",
+    "description": "ES6 JSON API Serializer Skeleton"
+  },
   "[ember-data] Transform Serializer Skeleton": {
     "prefix": "transform",
     "body": "import Transform from '@ember-data/serializer/transform';\n\nexport default Transform.extend({\n\t${1:// body}\n});",
     "description": "Transform Serializer Skeleton"
+  },
+  "[ember-data] ES6 Transform Serializer Skeleton": {
+    "prefix": "transform",
+    "body": "import classic from 'ember-classic-decorator';\nimport Transform from '@ember-data/serializer/transform';\n\n@classic\nclass ${1}Transform extends Transform {\n\t${2}\n}\n\nexport default ${1}Transform;",
+    "description": "ES6 Transform Serializer Skeleton"
   }
 };
--- src/javascript/ember/skeletons.js
+++ src/javascript/ember/skeletons.js
@@ -4,30 +4,65 @@ module.exports = {
     "body": "import Route from '@ember/routing/route';\n\nexport default Route.extend({\n\t${1:// body}\n});",
     "description": "Route Skeleton"
   },
+  "[ember] ES6 Route Skeleton": {
+    "prefix": "service",
+    "body": "import classic from 'ember-classic-decorator';\nimport Route from '@ember/routing/route';\n\n@classic\nclass ${1}Route extends Route {\n\t${2}\n}\n\nexport default ${1}Route;",
+    "description": "ES6 Route Skeleton"
+  },
   "[ember] Controller Skeleton": {
     "prefix": "controller",
     "body": "import Controller from '@ember/controller';\n\nexport default Controller.extend({\n\t${1:// body}\n});",
     "description": "Controller Skeleton"
+  },
+  "[ember] ES6 Controller Skeleton": {
+    "prefix": "service",
+    "body": "import classic from 'ember-classic-decorator';\nimport Controller from '@ember/controller';\n\n@classic\nclass ${1}Controller extends Controller {\n\t${2}\n}\n\nexport default ${1}Controller;",
+    "description": "ES6 Controller Skeleton"
   },
   "[ember] Service Skeleton": {
     "prefix": "service",
     "body": "import Service from '@ember/service';\n\nexport default Service.extend({\n\t${1:// body}\n});",
     "description": "Service Skeleton"
   },
-  "[ember] Component Skeleton": {
+  "[ember] ES6 Service Skeleton": {
+    "prefix": "service",
+    "body": "import classic from 'ember-classic-decorator';\nimport Service from '@ember/service';\n\n@classic\nclass ${1}Service extends Service {\n\t${2}\n}\n\nexport default ${1}Service;",
+    "description": "ES6 Service Skeleton"
+  },
+  "[ember] Classic Component Skeleton": {
     "prefix": "component",
-    "body": "import Component from '@ember/component';\n\nexport default Component.extend({\n\t${1:// body}\n});",
+    "body": "import Component from '@ember/component';\n\nexport default Component.extend({\n\ttagName: ''\n});",
     "description": "Component Skeleton"
+  },
+  "[ember] Glimmer Component Skeleton": {
+    "prefix": "component",
+    "body": "import Component from '@glimmer/component';\n\nclass ${1}Component extends Component {\n\t${2}\n}",
+    "description": "Glimmer Component Skeleton"
+  },
+  "[ember] ES6 Classic Component Skeleton": {
+    "prefix": "component",
+    "body": "import classic from 'ember-classic-decorator';\nimport Component from '@ember/component';\n\n@classic\nclass ${1}Component extends Component {\n\ttagName = '';\n}\n\nexport default ${1}Component;",
+    "description": "ES6 Classic Component Skeleton"
   },
   "[ember] Mixin Skeleton": {
     "prefix": "mixin",
     "body": "import Mixin from '@ember/object/mixin';\n\nexport default Mixin.create({\n\t${1:// body}\n});",
     "description": "Mixin Skeleton"
   },
+  "[ember] ES6 Mixin Skeleton": {
+    "prefix": "mixin",
+    "body": "import classic from 'ember-classic-decorator';\nimport Mixin from '@ember/object/mixin';\n\n@classic\nclass ${1}Mixin extends Mixin {\n\t${2}\n}\n\nexport default ${1}Mixin;",
+    "description": "ES6 Mixin Skeleton"
+  },
   "[ember] Object Skeleton": {
     "prefix": "object",
     "body": "import EmberObject from '@ember/object';\n\nexport default EmberObject.extend({\n\t${1:// body}\n});",
     "description": "Object Skeleton"
+  },
+  "[ember] ES6 Object Skeleton": {
+    "prefix": "object",
+    "body": "import classic from 'ember-classic-decorator';\nimport EmberObject from '@ember/object';\n\n@classic\nclass ${1} extends EmberObject {\n\t${2}\n}\n\nexport default ${1};",
+    "description": "ES6 Object Skeleton"
   },
   "[ember] Bound Helper Skeleton": {
     "prefix": "bound-helper",
@@ -38,6 +73,11 @@ module.exports = {
     "prefix": "helper",
     "body": "import Helper from '@ember/component/helper';\n\nexport default Helper.extend({\n\tcompute() {\n\t\t${1:// body}\n\t}\n});",
     "description": "Helper Skeleton"
+  },
+  "[ember] ES6 Helper Skeleton": {
+    "prefix": "helper",
+    "body": "import classic from 'ember-classic-decorator';\nimport Helper from '@ember/component/helper';\n\n@classic\nclass ${1}Helper extends Helper {\n\tcompute() {\n\t\t${2}\n\t}\n}\n\nexport default ${1}Helper;",
+    "description": "ES6 Helper Skeleton"
   },
   "[ember] Initializer Skeleton": {
     "prefix": "initializer",
